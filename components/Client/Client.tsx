93c01af642fc060dfd9e2b1b9731d1b6331a797b
Hina Alvi <princess4177812@gmail.com>
2024-12-16 04:40:02 -0800

    updated

---
+++ components/Client/Client.tsx
@@ -0,0 +1,62 @@
+
+
+
+
+import Image from "next/image"
+import client from "@/public/client1.jpg"
+import serverclient from "@/public/serverclient.jpg"
+import serverclient2 from "@/public/serverclient2.jpg"
+import Back from "@/public/back.png"
+export default function Client(){
+    return(
+        <div>
+            <div className="w-[1000px] h-[70px] bg-slate-400 ml-[200px] mt-[20px]">
+                <h1 className="text-center text-3xl pt-3 font-bold text-">Client Computer</h1>
+            </div>
+       <Image src={client} alt="client" className="w-[800px] h-[300px] ml-[270px] mt-[50px]"/>
+       {/* Ya ziyadda tar user ka liya use hota ha */}
+       {/* Client aik normal computer hota ha */}
+       <h1 className="text-center text-extrabold text-4xl ">What is Client?</h1>
+     <p className="w-[800px] ml-[270px] mt-[40px]">
+
+A client computer is a device that accesses services, data, or applications provided by a server in a network. It can be a desktop, laptop, smartphone, or any other device connected to the network. The primary role of a client computer is to send requests to the server and display or process the responses received. For example, when you browse a website, your computer (the client) requests the web page from a web server and displays it on your screen. Client computers typically have limited processing power compared to servers, as they are designed for end-user tasks such as browsing, editing documents, or running applications. 
+Examples of client computers include personal PCs, tablets, and mobile phones.
+     </p>
+
+
+       {/*Advantage of client  */}
+       <h1 className="text-center font-bold text-4xl mt-[60px]">Advantage of Client</h1>
+              <p className="text-center mt-[20px]">1: Ease of Use</p>
+              <p className="text-center">2: Cost-Effective</p>
+              <p className="text-center">3: Portability </p>
+              <p className="text-center">4: Security and Control</p>
+              <p className="text-center">5: Resource Access:</p>
+
+
+            {/*Disadvantage of client  */}
+       <h1 className="text-center font-bold text-4xl mt-[60px]">Disadvantage of Client</h1>
+              <p className="text-center mt-[20px]">1: Limited Processing Power</p>
+              <p className="text-center">2: Dependency on Servers</p>
+              <p className="text-center">3: Data Security Risks </p>
+              <p className="text-center">4: Storage Limitations</p>
+              <p className="text-center">5: Network Dependency</p>
+
+
+              <h1 className="text-center font-bold text-4xl mt-[60px]">Types of Client</h1>
+              <p className="text-center mt-[20px]">1: Smart Clients</p>
+              <p className="text-center">2: Web Client</p>
+              <p className="text-center">3:Mobile Client</p>
+
+              <div className="w-[1000px] h-[70px] bg-slate-400 ml-[200px] mt-[20px]">
+              <h1 className="text-center font-serif text-bold text-4xl">server & Client </h1>
+             </div>
+
+
+             <Image src={serverclient} alt="pic" className="w-[800px] h-[300px] ml-[260px]"/>
+             <Image src={serverclient2} alt="pic2"  className="w-[800px] h-[300px] ml-[260px] mt-[80px]"/>
+             {/* <Image src={Back} alt="back" className="w-full h-[300px]"/> */}
+
+
+        </div>
+    )
+}
---
+++ components/Server/Server.tsx
@@ -0,0 +1,59 @@
+
+
+import Image from "next/image";
+import server from "@/public/what is server.jpg"
+import typeserver from "@/public/typeserver.webp"
+import Daigram from "@/public/diagram.png"
+import Link from "next/link";
+
+export default function Server(){
+    return(
+        <div>
+            
+              
+                <div className="w-[1000px] h-[70px] bg-slate-400 ml-[200px] mt-[20px]">
+                <h1 className="text-center text-3xl pt-3 font-bold text-">Server Computer</h1>
+            </div>
+            <div className="w-[1000px] h-auto ml-[200px] ">
+              <Image src={server} alt="What is server" className="w-[800px] mt-[40px] h-[300px] ml-[110px]"/>
+              <h1 className="text-center text-extrabold text-4xl ">What is Server?</h1>
+              {/* Server ka kam ya hota ha ka agr koi client  os sa request krta hai to wo osa response karta ha */}
+              <p className="text-center text-[20px] ">A Server is a Computer that Provides Services to the computers and other devices connected to a Network..</p>
+              <p className="text-center text-[20px] ">Server computer is more powerfull then other computers in the Network..</p>
+
+              <Image src={typeserver} alt="TYPE SERVER" className="w-[800px] h-[300px] ml-[110px] mt-[60px]"/>
+              
+               {/* Type of server */}
+               <h1 className="text-center font-bold text-4xl mt-[30px]">Type of Server</h1>
+              <p className="text-center">1: File Server</p>
+              <p className="text-center">2: Application Server</p>
+              <p className="text-center">3: Mail Server</p>
+              <p className="text-center">4: Web Server</p>
+              <p className="text-center">5: Database Server</p>
+
+              {/*Advantage  */}
+              <h1 className="text-center font-bold text-4xl mt-[60px]">Advantage of Server</h1>
+              <p className="text-center mt-[20px]">1: Backup</p>
+              <p className="text-center">2: Security</p>
+              <p className="text-center">3: Storage</p>
+
+              {/* Disadvantage */}
+              {/* Agr server system fail hojaye tu ya hamara pora network system Destroy hojta ha.. */}
+              <h1 className="text-center font-bold text-4xl mt-[30px]">Disadvantage of Server</h1>
+              <p className="text-center">1: Server</p>
+              <p className="text-center">2: More Expensive</p>
+
+
+              <Image src={Daigram} alt="Daigram" className="ml-[100px] mt-[70px] w-[800px] h-[300px]"/>
+              <p className="text-center items-center mt-[20px] text-[20px] w-[700px] ml-[150px]">A server is a powerful computer or system that provides data, services, or resources to other computers, called clients, over a network. It is designed to handle multiple requests simultaneously and is often used to host websites, manage databases, store files, or run applications. Servers operate 24/7 to ensure consistent availability and reliability. Examples of servers include web servers, email servers, and file servers, which enable users to access specific resources or services remotely</p>
+              {/* <button className="px-6 py-2 flex justify-center items-center text-white bg-blue-500 rounded-md hover:bg-blue-600 transition duration-300">
+              <a href="./Client" className="px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 transition duration-300">
+    About Client
+  </a>
+
+  </button> */}
+              </div>
+        </div>
+        
+    )
+}
